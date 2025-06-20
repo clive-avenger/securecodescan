7caa0fe509da04eccaae279c90090d5f94b5a655
clive-avenger <clivenyamweya@gmail.com>
2025-06-20 09:19:28 +0300

    Create Report Generator.py

---
+++ Report Generator.py
@@ -0,0 +1,7 @@
+import csv
+
+def generate_csv(results, filename='report.csv'):
+    with open(filename, 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=results[0].keys())
+        writer.writeheader()
+        writer.writerows(results)
